815a5261db8d08c015950ae9f823426b9f176c74
Akiraccy <sos313423030@gmail.com>
2021-10-03 14:13:39 +0700

    Add project files.

---
+++ salon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="salon.Global" Language="C#" %>
---
+++ salon/login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace salon
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data source=Kasira-FEW;Initial Catalog=salon;Integrated Security=True; ");
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from tbl_member where username_id='" + TextBox1.Text + "'and password='" + TextBox2.Text + "' ", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                Session["username"] = TextBox1.Text;
+                Response.Redirect("home.aspx");
+            }
+        }  
+    
+    }
+}
